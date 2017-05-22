97a6368e194210d60e1d5aa0ad4b4ec0529afd8f
Raul Alejandro <Raul Alejandro>
2017-05-22 18:34:15 -0500

    quinto problema

---
+++ Problema5/diagonal.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int sum1=0, sum2 =0;
+        int a[][] = new int[n][n];
+        for(int a_i=0; a_i < n; a_i++){
+            for(int a_j=0; a_j < n; a_j++){
+                a[a_i][a_j] = in.nextInt();
+            }
+            sum2+= a[a_i][n-a_i-1];
+            sum1+= a[a_i][a_i];
+             
+        }
+        sum1= Math.abs(sum1-sum2);
+        System.out.println(sum1);
+    }
+}
